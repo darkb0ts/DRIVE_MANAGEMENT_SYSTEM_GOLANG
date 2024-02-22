fe97dd8751555005d268e8b6089152f384c294f2
darkb0ts <prasanna110600@gmail.com>
2024-02-22 10:02:05 +0530

    html style dir and javascript dir

--- api/api_fetch.go
+++ api/api_fetch.go
@@ -11,57 +11,57 @@ import (
 
 // Drive struct represents the data model for a drive
 type Drive struct {
-    Name          string `json:"name"`
-    VehicleNumber string `json:"vehicle_number"`
+	Name          string `json:"name"`
+	VehicleNumber string `json:"vehicle_number"`
 }
 
 var db *sql.DB
 
 func main() {
-    // Connect to the database
-    var err error
-    db, err = sql.Open("mysql", "######################")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
+	// Connect to the database
+	var err error
+	db, err = sql.Open("mysql", "######################")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-    // Initialize Gin
-    router := gin.Default()
+	// Initialize Gin
+	router := gin.Default()
 
-    // Define API endpoints
-    router.GET("/drives", getDrives)
+	// Define API endpoints
+	router.GET("/drives", getDrives)
 
-    // Run the server
-    router.Run(":8080")
+	// Run the server
+	router.Run(":8080")
 }
 
 func getDrives(c *gin.Context) {
-    // Query the database to get drive names and vehicle numbers
-    rows, err := db.Query("SELECT name, vehicle_number FROM drives")
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        return
-    }
-    defer rows.Close()
+	// Query the database to get drive names and vehicle numbers
+	rows, err := db.Query("SELECT name, vehicle_number FROM drives")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
 
-    // Iterate over the rows and populate a slice of Drive structs
-    var drives []Drive
-    for rows.Next() {
-        var drive Drive
-        if err := rows.Scan(&drive.Name, &drive.VehicleNumber); err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-            return
-        }
-        drives = append(drives, drive)
-    }
+	// Iterate over the rows and populate a slice of Drive structs
+	var drives []Drive
+	for rows.Next() {
+		var drive Drive
+		if err := rows.Scan(&drive.Name, &drive.VehicleNumber); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		drives = append(drives, drive)
+	}
 
-    // Check for errors during iteration
-    if err := rows.Err(); err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        return
-    }
+	// Check for errors during iteration
+	if err := rows.Err(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
-    // Return the list of drives as JSON
-    c.JSON(http.StatusOK, drives)
+	// Return the list of drives as JSON
+	c.JSON(http.StatusOK, drives)
 }
